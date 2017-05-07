8944c256a4dbe9c3ae4697af8288443ec20e7179
Rikesh Thapa <rthapa@u.rochester.edu>
2017-05-06 23:34:57 -0700

    Fibbonacci fix, code optimized to be O(n)

--- 0AlgorithmToolbox/Project1/fibonacci/fibonacci.py
+++ 0AlgorithmToolbox/Project1/fibonacci/fibonacci.py
@@ -1,5 +1,7 @@
 # Uses python3
-def calc_fib(n):
+#This is an extremely naieve solution, takes O(n^2) time to complete, because of
+# recursive startegy
+'''def calc_fib(n):
     if (n <= 1):
         return n
 
@@ -7,3 +9,25 @@ def calc_fib(n):
 
 n = int(input())
 print(calc_fib(n))
+'''
+
+def calc_fib(n):
+    finarr = []
+    i =0
+    while i<=n:
+        if i==0:
+            finarr.append(0)
+        elif i==1:
+            finarr.append(1)
+        else:
+            newVal = finarr[i-1]+finarr[i-2]
+            finarr.append(newVal)
+        i +=1
+    return finarr[n]
+
+n= int(input())
+print(calc_fib(n))
+    
+
+
+

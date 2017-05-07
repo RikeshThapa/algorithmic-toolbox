7ccaf576908671c9b15006eb6d03281e09cfeceb
Rikesh Thapa <rthapa@u.rochester.edu>
2017-05-07 01:25:23 -0700

    adding euclids algo for gcd instead of naieve, makes timeconstant o(N) instead of some other crazy complexity

--- 0AlgorithmToolbox/Project1/fibonacci_last_digit/fibonacci_last_digit.py
+++ 0AlgorithmToolbox/Project1/fibonacci_last_digit/fibonacci_last_digit.py
@@ -1,5 +1,4 @@
 # Uses python3
-import sys
 
 def get_fibonacci_last_digit_naive(n):
     if n <= 1:
@@ -8,12 +7,13 @@ def get_fibonacci_last_digit_naive(n):
     previous = 0
     current  = 1
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
+    i=0
 
+    while i<(n-1):
+        #Here is the simple fix to make the program, 
+        previous, current = current, (previous + current)%10
+        i+=1
     return current % 10
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+n = int(input())
+print(get_fibonacci_last_digit_naive(n))
--- 0AlgorithmToolbox/Project1/gcd/gcd.py
+++ 0AlgorithmToolbox/Project1/gcd/gcd.py
@@ -1,16 +1,28 @@
 # Uses python3
-import sys
 
+'''
 def gcd_naive(a, b):
     current_gcd = 1
     for d in range(2, min(a, b) + 1):
+        print(d)
         if a % d == 0 and b % d == 0:
             if d > current_gcd:
                 current_gcd = d
 
+    return current_gcd'''
+
+#new mods should give O(n)
+def gcd_euclid(a, b):
+    small = min(a,b)
+    big = max(a,b)
+    current_gcd = small
+    mod = big%small
+    while mod!=0:        
+        new_gcd = mod
+        mod = current_gcd%mod
+        current_gcd = new_gcd
     return current_gcd
 
-if __name__ == "__main__":
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+input = input()
+a, b = map(int, input.split())
+print(gcd_euclid(a, b))

642e169670c7528edc8b77eae0350fc55e880e33
Rikesh Thapa <rthapa@u.rochester.edu>
2017-05-15 00:14:45 -0700

    stack implementation for compiler check of bracket validity

--- 1DataStructures/Week1/check_brackets_in_code/check_brackets.py
+++ 1DataStructures/Week1/check_brackets_in_code/check_brackets.py
@@ -20,13 +20,21 @@ if __name__ == "__main__":
     text = sys.stdin.read()
 
     opening_brackets_stack = []
+    success = 1
     for i, next in enumerate(text):
         if next == '(' or next == '[' or next == '{':
-            # Process opening bracket, write your code here
-            pass
+            opening_bracket_stack.append(next)
 
         if next == ')' or next == ']' or next == '}':
-            # Process closing bracket, write your code here
-            pass
-
-    # Printing answer, write your code here
+            bracket = Bracket(opening_bracket_stack.pop(), i-1)
+            if bracket.Match(next):
+                pass
+            else:
+                index = i
+                success = 0
+                break
+                
+    if success:
+        print("Success")
+    else:
+        print(index)

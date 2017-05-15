65dbf02c024b5eee8167ee975335e109b3744deb
Rikesh Thapa <rthapa@u.rochester.edu>
2017-05-15 10:58:04 -0700

    check_brackets fixed for corner cases

--- 1DataStructures/Week1/check_brackets_in_code/check_brackets.py
+++ 1DataStructures/Week1/check_brackets_in_code/check_brackets.py
@@ -23,18 +23,35 @@ if __name__ == "__main__":
     success = 1
     for i, next in enumerate(text):
         if next == '(' or next == '[' or next == '{':
-            opening_bracket_stack.append(next)
+            bracket = Bracket(next, i+1)
+            opening_brackets_stack.append(bracket)
+            continue
 
         if next == ')' or next == ']' or next == '}':
-            bracket = Bracket(opening_bracket_stack.pop(), i-1)
-            if bracket.Match(next):
-                pass
+            nextBracket = Bracket(next, i+1)
+            if opening_brackets_stack!=[]:
+                bracket = opening_brackets_stack.pop()
+            elif i==0:
+                success=0
+                index=1
+                break
             else:
-                index = i
+                success=0
+                index=nextBracket.position
+                break
+            
+            if bracket.Match(nextBracket.bracket_type):
+                continue
+            else:
+                index = nextBracket.position
                 success = 0
                 break
-                
-    if success:
-        print("Success")
-    else:
+    #Final check to see if all brackets have been satisfied
+    if success==0:
         print(index)
+    elif opening_brackets_stack != []:
+        mismatch = opening_brackets_stack.pop()
+        print(mismatch.position)
+    else:
+        print("Success")
+        

4d7d5a065e5fb43cf4d3cd46dfe1d8e1d2a9d038
Rikesh Thapa <rthapa@u.rochester.edu>
2017-05-16 23:17:49 -0700

    faulty tree depth calculator

--- 1DataStructures/Week1/tree_height/tree-height.py
+++ 1DataStructures/Week1/tree_height/tree-height.py
@@ -7,19 +7,26 @@ threading.stack_size(2**27)  # new thread will get stack of such size
 class TreeHeight:
         def read(self):
                 self.n = int(sys.stdin.readline())
+                #converts everything into int and puts into a list
                 self.parent = list(map(int, sys.stdin.readline().split()))
 
         def compute_height(self):
                 # Replace this code with a faster implementation
                 maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
+                traversedSet = []
+                while True:
+                        if(traversedSet.count(self.parent.index(-1))<=self.parent.count(self.parent.index(-1))):
+                                vertex = self.parent.index(-1)
+                        else:
+                                return maxHeight
+
+                        while((vertex in self.parent) and (traversedSet.count(vertex)<=self.parent.count(vertex))):
                                 height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+                                traversedSet.append(vertex)
+                                vertex = self.parent.index(vertex)
+                        traversedSet.append(vertex)
+                        maxHeight = max(height, maxHeight)
+                        height = 0
 
 def main():
   tree = TreeHeight()
